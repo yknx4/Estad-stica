d54cf523d094404f9ad5d0fac142fc9f5942d8a4
Jorge Figueroa (Qosmio W8) <yknx4@outlook.com>
2014-03-23 11:09:44 -0600

    fc

---
+++ MAE/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace MAE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAddEstrato = new System.Windows.Forms.Button();
+            this.cmbEstratos = new System.Windows.Forms.ComboBox();
+            this.lstEstratos = new System.Windows.Forms.ListBox();
+            this.txtDatos = new System.Windows.Forms.TextBox();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.txtAddV = new System.Windows.Forms.Button();
+            this.txtTamaño = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTabla = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPreParaM = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnAddEstrato
+            // 
+            this.btnAddEstrato.Location = new System.Drawing.Point(13, 13);
+            this.btnAddEstrato.Name = "btnAddEstrato";
+            this.btnAddEstrato.Size = new System.Drawing.Size(99, 23);
+            this.btnAddEstrato.TabIndex = 0;
+            this.btnAddEstrato.Text = "Agregar Estrato";
+            this.btnAddEstrato.UseVisualStyleBackColor = true;
+            this.btnAddEstrato.Click += new System.EventHandler(this.btnAddEstrato_Click);
+            // 
+            // cmbEstratos
+            // 
+            this.cmbEstratos.FormattingEnabled = true;
+            this.cmbEstratos.Location = new System.Drawing.Point(13, 43);
+            this.cmbEstratos.Name = "cmbEstratos";
+            this.cmbEstratos.Size = new System.Drawing.Size(99, 21);
+            this.cmbEstratos.TabIndex = 1;
+            this.cmbEstratos.SelectedIndexChanged += new System.EventHandler(this.cmbEstratos_SelectedIndexChanged);
+            // 
+            // lstEstratos
+            // 
+            this.lstEstratos.FormattingEnabled = true;
+            this.lstEstratos.Location = new System.Drawing.Point(13, 70);
+            this.lstEstratos.Name = "lstEstratos";
+            this.lstEstratos.Size = new System.Drawing.Size(99, 381);
+            this.lstEstratos.TabIndex = 2;
+            // 
+            // txtDatos
+            // 
+            this.txtDatos.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtDatos.Location = new System.Drawing.Point(118, 15);
+            this.txtDatos.Multiline = true;
+            this.txtDatos.Name = "txtDatos";
+            this.txtDatos.Size = new System.Drawing.Size(336, 470);
+            this.txtDatos.TabIndex = 3;
+            this.txtDatos.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.refrescar);
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(13, 464);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(67, 20);
+            this.txtValor.TabIndex = 4;
+            this.txtValor.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.CheckKeys);
+            // 
+            // txtAddV
+            // 
+            this.txtAddV.Location = new System.Drawing.Point(86, 462);
+            this.txtAddV.Name = "txtAddV";
+            this.txtAddV.Size = new System.Drawing.Size(26, 23);
+            this.txtAddV.TabIndex = 5;
+            this.txtAddV.Text = "+";
+            this.txtAddV.UseVisualStyleBackColor = true;
+            this.txtAddV.Click += new System.EventHandler(this.txtAddV_Click);
+            // 
+            // txtTamaño
+            // 
+            this.txtTamaño.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtTamaño.Location = new System.Drawing.Point(43, 491);
+            this.txtTamaño.Name = "txtTamaño";
+            this.txtTamaño.Size = new System.Drawing.Size(73, 20);
+            this.txtTamaño.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 494);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "N=";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(122, 494);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Valor de tabla =";
+            // 
+            // txtTabla
+            // 
+            this.txtTabla.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtTabla.Location = new System.Drawing.Point(209, 491);
+            this.txtTabla.Name = "txtTabla";
+            this.txtTabla.Size = new System.Drawing.Size(73, 20);
+            this.txtTabla.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(288, 494);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "% para est media";
+            // 
+            // txtPreParaM
+            // 
+            this.txtPreParaM.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPreParaM.Location = new System.Drawing.Point(381, 491);
+            this.txtPreParaM.Name = "txtPreParaM";
+            this.txtPreParaM.Size = new System.Drawing.Size(73, 20);
+            this.txtPreParaM.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(466, 526);
+            this.Controls.Add(this.txtPreParaM);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtTabla);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTamaño);
+            this.Controls.Add(this.txtAddV);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.txtDatos);
+            this.Controls.Add(this.lstEstratos);
+            this.Controls.Add(this.cmbEstratos);
+            this.Controls.Add(this.btnAddEstrato);
+            this.Name = "Form1";
+            this.Text = "Muestreo Aleatorio Estratificado";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAddEstrato;
+        private System.Windows.Forms.ComboBox cmbEstratos;
+        private System.Windows.Forms.ListBox lstEstratos;
+        private System.Windows.Forms.TextBox txtDatos;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.Button txtAddV;
+        private System.Windows.Forms.TextBox txtTamaño;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTabla;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPreParaM;
+    }
+}
+
---
+++ MAE/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MAE
+{
+    public partial class Form1 : Form
+    {
+        BindingList<BindingList<double>> estratos = new BindingList<BindingList<double>>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            estratos.Add(new BindingList<double>());
+            cmbEstratos.DataSource = estratos;
+            lstEstratos.DataSource = estratos[0];
+        }
+        private void CheckKeys(object sender, System.Windows.Forms.KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+            {
+                estratos[cmbEstratos.SelectedIndex].Add(double.Parse(txtValor.Text));
+                txtValor.Clear();
+            }
+        }
+        private void txtAddV_Click(object sender, EventArgs e)
+        {
+            estratos[cmbEstratos.SelectedIndex].Add(double.Parse(txtValor.Text));
+            txtValor.Clear();
+        }
+
+        private void cmbEstratos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lstEstratos.DataSource = estratos[cmbEstratos.SelectedIndex];
+        }
+
+        private void btnAddEstrato_Click(object sender, EventArgs e)
+        {
+            estratos.Add(new BindingList<double>());
+            cmbEstratos.DataSource = estratos;
+        }
+
+        private double calcularPromedioPorEstrato(BindingList<double> datos)
+        {
+            double suma = 0;
+            foreach (double val in datos)
+            {
+                suma += val;
+            }
+            return suma / datos.Count;
+
+        }
+        private double calcularPromedioTotal(params double[] datos)
+        {
+            double suma = 0;
+            foreach (double val in datos)
+            {
+                suma += val;
+            }
+            return suma / datos.Count();
+
+        }
+
+        private void refrescar(object sender, MouseEventArgs e)
+        {
+            double countTotal = 0;
+            int N =int.Parse(txtTamaño.Text);
+            double valorTabla = double.Parse(txtTabla.Text);
+            double preParaEstMed = double.Parse(txtPreParaM.Text);
+            List<double> promedios = new List<double>();
+            List<double> varianzas = new List<double>();
+            List<double> valoresDeMuestra_n = new List<double>();
+            List<double> valoresDeMuestra_nf = new List<double>();
+            txtDatos.Clear();
+            int count = 1;
+            //Calculo de promedio por estrato
+            txtDatos.AppendText("A)" + Environment.NewLine);
+            foreach (var lista in estratos)
+            {
+                countTotal += lista.Count;
+                double pro = calcularPromedioPorEstrato(lista);
+                promedios.Add(pro);
+                txtDatos.AppendText("Promedio estrato " + count + " : "+pro.ToString()+Environment.NewLine);
+                count++;
+            }
+            double promedioEstr =0;
+            count =0;
+            //Calculo de promedio estratificado
+            foreach (var lista in estratos)
+            {
+                promedioEstr += promedios[count]*((lista.Count/countTotal)*N);
+                    count++;
+            }
+            
+            promedioEstr /= N;
+            txtDatos.AppendText("Promedio estratificado : " + promedioEstr.ToString() + Environment.NewLine);
+            //Calculo de estimación puntual
+            txtDatos.AppendText("B)" + Environment.NewLine);
+            double estPunt = promedioEstr * N;
+            txtDatos.AppendText("T^ = " + estPunt.ToString() + Environment.NewLine);
+            count = 0;
+            //Calculo de Varianzas por estrato
+            txtDatos.AppendText("C)" + Environment.NewLine);
+            foreach (var lista in estratos)
+            {
+                double currVar = 0;
+                foreach (double val in lista)
+                {
+                    double calc = val - promedios[count];
+                    currVar += (calc * calc);
+                }
+                currVar /= (lista.Count - 1);
+                varianzas.Add(currVar);
+                txtDatos.AppendText("Varianza estrato " + (count + 1) + " : " + currVar.ToString() + Environment.NewLine);
+                count++;
+            }
+            //Calculo de varianza total y desviación estandar;
+            double varianzaEstr = 0;
+            double desvEstr = 0;
+            count = 0;
+            
+            foreach (var lista in estratos)
+            {
+                double Nh = (lista.Count / countTotal) * N;
+                varianzaEstr += (Nh*Nh)*((Nh-lista.Count)/Nh)*(varianzas[count]/lista.Count);
+                count++;
+            }
+
+            varianzaEstr /= (N*N);
+            txtDatos.AppendText("Varianza estratificada : " + varianzaEstr.ToString() + Environment.NewLine);
+            desvEstr = Math.Sqrt(varianzaEstr);
+            txtDatos.AppendText("Desv Est estratificada : " + desvEstr.ToString() + Environment.NewLine);
+
+            //Intervalo de confianza
+            txtDatos.AppendText("D)" + Environment.NewLine);
+            double intConf = valorTabla*desvEstr;
+            txtDatos.AppendText((promedioEstr - intConf) + " <= Uest <= " + (promedioEstr + intConf) + Environment.NewLine);
+            //Intervalo de confianza2
+            txtDatos.AppendText("E)" + Environment.NewLine);
+            double valNeceInt = varianzaEstr * N * N;
+            double intConf2 = valorTabla * Math.Sqrt(valNeceInt);
+            txtDatos.AppendText((estPunt - intConf2) + " <= T^ <= " + (estPunt + intConf2) + Environment.NewLine);
+            //Tamaño de muestra
+            txtDatos.AppendText("F)" + Environment.NewLine);
+            double nFi = 0;
+            double whsh2=0;
+            count = 0;
+            foreach (var lista in estratos)
+            {
+                double Nh = (lista.Count / countTotal);
+                whsh2 += Nh * varianzas[count];
+                count++;
+            }
+            double d = preParaEstMed * promedioEstr;
+            nFi = (N * valorTabla * valorTabla * whsh2) / ((N * d * d) + (valorTabla * valorTabla * whsh2));
+            txtDatos.AppendText("(tamaño muestra) n : " + nFi.ToString() + Environment.NewLine);
+            count = 0;
+            foreach (var lista in estratos)
+            {
+                double Nh = (lista.Count / countTotal) * nFi;
+                valoresDeMuestra_n.Add(Nh);
+                txtDatos.AppendText("(tamaño muestra) n "+(count+1)+": " + Nh.ToString() + Environment.NewLine);
+                count++;
+            }
+            //Tamaño de muestra
+            txtDatos.AppendText("G)" + Environment.NewLine);
+            double nFi2 = 0;
+            double d2 = preParaEstMed * estPunt;
+            nFi2 = (N*N * valorTabla * valorTabla * whsh2) / ((d2 * d2) + (valorTabla * valorTabla * whsh2));
+            txtDatos.AppendText("(tamaño muestra final) n : " + nFi2.ToString() + Environment.NewLine);
+            count = 0;
+            foreach (var lista in estratos)
+            {
+                double Nh = (lista.Count / countTotal) * nFi2;
+                valoresDeMuestra_nf.Add(Nh);
+                txtDatos.AppendText("(tamaño muestra final) n " + (count + 1) + ": " + Nh.ToString() + Environment.NewLine);
+                count++;
+            }
+        }
+    }
+}
